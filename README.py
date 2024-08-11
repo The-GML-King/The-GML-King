8ff1b702f672748c057e8aa9f0b909038f2983fb
KARLx <nomedenelimternet@gmail.com>
2024-08-11 13:08:36 -0500

    Create README.py

---
+++ README.py
@@ -0,0 +1,11 @@
+'''
+    This Is My profile you dumbasses
+    sorry thought
+    I <3 you hackers
+    uhm look at this code below
+'''
+num = ""
+while num != "*****":
+  num += "*"
+else:
+  print("Limit exceeded")
